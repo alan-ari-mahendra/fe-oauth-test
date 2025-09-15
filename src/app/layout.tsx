c1440b5d23a74115c37ebf1b75fb92737ecac1a9
Alan Ari <mail.alanari@gmail.com>
2025-09-15 10:50:32 +0700

    feat: init material UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import {AppRouterCacheProvider} from "@mui/material-nextjs/v13-appRouter";
+import { ThemeProvider } from "@mui/material/styles"
+import CssBaseline from "@mui/material/CssBaseline"
+import theme from "@/theme";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +31,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+      <AppRouterCacheProvider options={{ key: "mui" }}>
+          <ThemeProvider theme={theme}>
+              <CssBaseline />
+                {children}
+          </ThemeProvider>
+      </AppRouterCacheProvider>
       </body>
     </html>
   );
---
+++ src/theme.ts
@@ -0,0 +1,18 @@
+"use client"
+
+import { createTheme } from "@mui/material/styles"
+
+const theme = createTheme({
+    cssVariables: true,
+    palette: {
+        mode: "light",
+        primary: {
+            main: "#1976d2",
+        },
+        secondary: {
+            main: "#9c27b0",
+        },
+    },
+})
+
+export default theme
